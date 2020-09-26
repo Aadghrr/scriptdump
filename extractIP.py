7aeccf71646895ef8422c71a824d0e6e13eb3f1d
Darragh Tobin <darraghtobin@gmail.com>
2020-09-26 09:41:53 +0100

    eIp

--- extractIP.py
+++ extractIP.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 import re,sys
-
-with open(sys.argv[1]) as fd:
-    res = fd.read()
-
-r = r'\t(?:[0-9]{1,3}\.){3}[0-9]{1,3}'
+res = ' '.join(sys.argv[1:])
+r = r'(?:[0-9]{1,3}\.){3}[0-9]{1,3}'
 for i in re.findall(r,res):
     print(i[1:])
