8651339b15e0713226ffaf300d4311fa5cdf19e1
dt <a@a.a>
2021-11-15 18:08:38 -0500

    handle lists of dicts

--- dictSchema.py
+++ dictSchema.py
@@ -3,3 +3,7 @@ def pc(x,i=0):
         for k in x:
             print(' '*i,'-'*i,k,sep='')
             pc(x[k],i+1)
+    elif type(x)==type(list()) and len(x)>0:
+        if type(x[0])==type(dict()) and len(x[0])!=0:
+            print(' '*i,'L')
+            self.schema(x[0],i)
